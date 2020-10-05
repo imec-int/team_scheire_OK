477e38a7bf8883a1fb549aecc1e8b264fecb751d
Wouter Devriese <wouter.devriese@imec.be>
2020-10-05 17:00:40 +0200

    code cleanup

--- arucoMap/src/main.cpp
+++ arucoMap/src/main.cpp
@@ -5,17 +5,7 @@
 int main()
 {
 
-    //ofGLESWindowSettings settings;
-    //settings.setSize(1024, 768);
-    //settings.setGLESVersion(2);
-
-    //ofCreateWindow(settings);
     ofSetupOpenGL(1024, 768, OF_FULLSCREEN);
 
-    // <-------- setup the GL context
-
-    // this kicks off the running of my app
-    // can be OF_WINDOW or OF_FULLSCREEN
-    // pass in width and height too:
     ofRunApp(new ofApp());
 }
